265f86b1cccc32700165291c5a274917bd5adf75
Newton Adhikari <newtonadk@gmail.com>
2026-03-18 13:54:31 +0545

    added sub projects disaster_world and disaster_robot

--- src/disaster_navigation/launch/simulation.launch.py
+++ src/disaster_navigation/launch/simulation.launch.py
@@ -1,7 +1,13 @@
 from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+import os
 
 
 def generate_launch_description():
+    disaster_world_pkg = get_package_share_directory('disaster_world')
+    disaster_robot_pkg = get_package_share_directory('disaster_robot')
+    disaster_nav_pkg   = get_package_share_directory('disaster_navigation')
+    gazebo_ros_pkg     = get_package_share_directory('gazebo_ros')
 
     return LaunchDescription([
         
