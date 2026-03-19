68997e8840bba33b539fcd4524d7a34b0c77bebf
Newton Adhikari <newtonadk@gmail.com>
2026-03-19 10:32:23 +0545

    minimal launch setup

--- src/disaster_navigation/launch/simulation.launch.py
+++ src/disaster_navigation/launch/simulation.launch.py
@@ -1,6 +1,12 @@
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 import os
+from launch.actions import (SetEnvironmentVariable,IncludeLaunchDescription, TimerAction, LogInfo)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -17,6 +23,66 @@ def generate_launch_description():
     rviz_config_file = os.path.join(disaster_nav_pkg,   'rviz',   'disaster_full.rviz')
     model_path       = os.path.join(disaster_world_pkg, 'models')
 
+    set_model_path = SetEnvironmentVariable(
+        'GAZEBO_MODEL_PATH',
+        [model_path, ':', os.path.expanduser('~/.gazebo/models')]
+    )
+
+    gzserver = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(gazebo_ros_pkg, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world_file, 'verbose': 'false'}.items()
+    )
+
+    gzclient = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(gazebo_ros_pkg, 'launch', 'gzclient.launch.py')
+        ),
+        condition=IfCondition(LaunchConfiguration('gui'))
+    )
+
+    robot_description = ParameterValue(
+        Command(['xacro ', urdf_file]),
+        value_type=str
+    )
+
+    robot_state_pub = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': robot_description,
+            'use_sim_time': True,
+        }]
+    )
+
+    # Spawn robot (delayed 3s to let Gazebo start)
+    spawn_robot = TimerAction(
+        period=3.0,
+        actions=[
+            Node(
+                package='gazebo_ros',
+                executable='spawn_entity.py',
+                name='spawn_robot',
+                output='screen',
+                arguments=[
+                    '-entity', 'disaster_robot',
+                    '-topic',  'robot_description',
+                    '-x', LaunchConfiguration('x'),
+                    '-y', LaunchConfiguration('y'),
+                    '-z', '1.0',   # ground plane is at z=1.0 in world — spawn AT the surface
+                    '-Y', LaunchConfiguration('yaw'),
+                ]
+            )
+        ]
+    )
+
     return LaunchDescription([
-        
+        set_model_path,
+        gzserver,
+        gzclient,
+        robot_state_pub,
+        spawn_robot
     ])
