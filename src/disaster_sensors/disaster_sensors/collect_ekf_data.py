fb43510cffa50ee97f215cb97063426096af35c7
Newton Adhikari <newtonadk@gmail.com>
2026-03-27 11:34:02 +0545

    added callback for filtered odometry

--- src/disaster_sensors/disaster_sensors/collect_ekf_data.py
+++ src/disaster_sensors/disaster_sensors/collect_ekf_data.py
@@ -19,6 +19,7 @@ class EKFDataCollector(Node):
         qos = 10
 
         self.create_subscription(Odometry, '/odom', self._raw_cb, qos)
+        self.create_subscription(Odometry, '/odometry/filtered', self._ekf_cb, qos)
 
     def _raw_cb(self, msg):
         t = self._ts(msg.header)
@@ -29,6 +30,18 @@ class EKFDataCollector(Node):
         
         # record the data
         self.raw_odom.append((t, msg.pose.pose.position.x, msg.pose.pose.position.y, self._yaw(msg.pose.pose.orientation)))
+
+    # helper method for timestamp
+    def _ts(self, header):
+        return header.stamp.sec + header.stamp.nanosec * 1e-9
+    
+    def _ekf_cb(self, msg):
+        if self.start_time is None:
+            return
+        t = self._ts(msg.header)
+
+        # filtered odom data
+        self.ekf_odom.append((t, msg.pose.pose.position.x, msg.pose.pose.position.y, self._yaw(msg.pose.pose.orientation)))
 
 
 def main():
