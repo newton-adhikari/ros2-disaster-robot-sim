1c86df88ebc8c99b8415d88ffe6b2fb4dcd1a54f
Newton Adhikari <newtonadk@gmail.com>
2026-03-27 11:31:51 +0545

    created call back for odometry

--- src/disaster_sensors/disaster_sensors/collect_ekf_data.py
+++ src/disaster_sensors/disaster_sensors/collect_ekf_data.py
@@ -1,10 +1,34 @@
 import rclpy
 from rclpy.node import Node
+import time
+
+from nav_msgs.msg import Odometry
+
 
 class EKFDataCollector(Node):
 
-    def __init__(self):
+    def __init__(self, duration: float):
         super().__init__('ekf_data_collector')
+        self.duration   = duration
+        self.raw_odom   = [] 
+        self.ekf_odom   = [] 
+        self.groundtruth= [] 
+        self.start_time = None
+
+        # using this to match robot_localization publisher
+        qos = 10
+
+        self.create_subscription(Odometry, '/odom', self._raw_cb, qos)
+
+    def _raw_cb(self, msg):
+        t = self._ts(msg.header)
+
+        if self.start_time is None:
+            self.start_time = time.time()
+            self.get_logger().info('Recording started — please have the robot running!')
+        
+        # record the data
+        self.raw_odom.append((t, msg.pose.pose.position.x, msg.pose.pose.position.y, self._yaw(msg.pose.pose.orientation)))
 
 
 def main():
